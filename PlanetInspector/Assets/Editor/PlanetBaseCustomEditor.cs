ccfc334d659e5601330d9c59eff18ff380e9d4a4
svc-student <svc-student@student.directory.jccc>
2017-03-27 20:11:35 -0500

    classwork3/27

---
+++ PlanetInspector/Assets/Editor/PlanetBaseCustomEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlanetBase))]
+public class PlanetBaseCustomEditor : Editor
+{
+    PlanetBase myTarget;
+    void OnEnable()
+    {
+        myTarget = (PlanetBase)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        //set center alignment for main segment labels 
+        GUIStyle centerer = GUI.skin.label;
+        centerer.alignment = TextAnchor.MiddleCenter;
+
+        //System Level Planet Generation
+        EditorGUILayout.BeginHorizontal("Box");
+        EditorGUILayout.LabelField("System Level Planet Generation", centerer);
+        //orbitTime(.1 - 300)
+        //revTime
+        //moonAmount(0-100)
+        EditorGUILayout.EndHorizontal();
+
+        //Planet Level Generation
+        EditorGUILayout.BeginHorizontal("Box");
+        EditorGUILayout.LabelField("Planet Level Generation", centerer);
+        //planet size
+        //mainColor
+        //min max slider for low and high elevation
+        //water
+        EditorGUILayout.EndHorizontal();
+
+        //Livability
+        EditorGUILayout.BeginHorizontal("Box");
+        EditorGUILayout.LabelField("Livability", centerer);
+        //habitable
+        //  hasFlora
+        //      hasFauna
+        //radiationAmount
+        //min max slider for low and high temp
+        //intelligentCreatues
+        //  icPopulation
+        EditorGUILayout.EndHorizontal();
+
+    }
+}
